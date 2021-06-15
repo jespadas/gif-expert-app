e4d04754f7f8d50375c4ae1a2ae216598a4e6d3c
jespadas <julio.espadas@gmail.com>
2021-06-14 19:24:30 -0500

    test code for GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,12 +4,45 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
 
-    test('Debe mostrar el componente correctamente', () => {
+    const title = "el titulo";
 
-        const wrapper = shallow(<GifGridItem />);
+    const url = "https://localhost:3000/img.png";
+
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
+
+    test('Debe mostrar el componente correctamente', () => {
 
         expect(wrapper).toMatchSnapshot();
 
+    })
+
+    test('Debe tener un parrafo con un parrafo en el titulo', () => {
+
+        const p = wrapper.find('p');
+
+        expect(p.text().trim()).toBe(title);
+
+    })
+
+    test('Debe tener la imagen igual al url y alt de los props', () => {
+
+        const img = wrapper.find(('img'));
+
+        // Para ver las props
+        //console.log(img.prop('src'));
+
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+
+    })
+
+    test('Debe de tener las clases animate__animated animate__zoomIn', () => {
+
+        const div = wrapper.find('div');
+
+        const className = div.prop('className');
+
+        expect(className.includes('animate__zoomIn')).toBe(true);
     })
 
 })
